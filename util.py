1775366a5521a168cdd83d36ae6c0d7431eb9f28
John-Olof Nilsson <john_nil@hotmail.com>
2025-08-07 23:25:34 +0200

    Add typing and an abstract base class for series classes.

--- util.py
+++ util.py
@@ -3,6 +3,8 @@
 """
 
 # External includes.
+import abc
+from collections.abc import Callable
 import sympy as sp
 from functools import lru_cache
 
@@ -10,17 +12,47 @@ from functools import lru_cache
 import polynomials
 from symbols import e2
 
-class Series:
+class SeriesBase(abc.ABC):
+    """Abstract base class for lazy series computations.
+
+    This class and the typing could really be removed, but it has been
+    introduced, at least partially, to support in C++ conversion.
+    """
+    @abc.abstractmethod
+    def __getitem__(self, n : int) -> sp.core.Expr:
+        """Compute the n:th series item.
+        :param n: Series index.
+        :return: n:th series item.
+        """
+        pass
+
+    @abc.abstractmethod
+    def __mul__(self, other : 'SeriesBase | sp.core.Expr') -> 'SeriesBase':
+        """Multiply series with series or factor.
+        :param other: Factor or series.
+        :return: resulting series.
+        """
+        pass
+
+    @abc.abstractmethod
+    def __add__(self, other : 'SeriesBase') -> 'SeriesBase':
+        """Add series to series.
+        :param other: Other series.
+        :return: resulting series.
+        """
+        pass
+
+
+class Series(SeriesBase):
     """Class for representing a series and implement related series arithmetics.
     """
 
-    def __init__(self, gen):
+    def __init__(self, gen : Callable[[int], sp.core.Expr]):
         self.gen = gen  # Function taking an integer and returning coefficient.
         # Add 16 items in the cache to start with.
-        self.items = [None]*16
+        self.items = [None] * 16
 
-    # Get the n:th term.
-    def __getitem__(self, n):
+    def __getitem__(self, n : int) -> sp.core.Expr:
         # Double length of items cache while too short.
         while len(self.items) <= n:
             self.items = self.items + [None]*(len(self.items))
@@ -29,53 +61,60 @@ class Series:
             self.items[n] = self.gen(n)
         # Return cached item.
         return self.items[n]
-        # return self.gen(n)
 
-    def __mul__(self, other):
+    def __mul__(self, other : SeriesBase | sp.core.Expr) -> SeriesBase:
         if isinstance(other, Series):
             # Cauchy product.
             return Series(lambda n: sum([self[l] * other[n - l] for l in range(n + 1)]))
-        else:
+        elif isinstance(other, SeriesFactor):
             # Series Factor
+            return Series(lambda n: self[n] * other[0])
+        else:
             return Series(lambda n: self[n] * other)
 
-    def __add__(self, other):
+    def __add__(self, other : SeriesBase) -> SeriesBase:
         return Series(lambda n: self[n] + other[n])
 
 
-class SeriesFactor:
+class SeriesFactor(SeriesBase):
     """Constant factor of a series as of The Series class.
     """
 
-    def __init__(self, a):
+    def __init__(self, a : sp.core.Expr):
         self.a = a
 
-    def __getitem__(self, n):
+    def __getitem__(self, n : int) -> sp.core.Expr:
         # A factor is also a series with only a constant term.
         if n == 0:
             return self.a
-        return 0
+        return sp.Integer(0)
 
-    def __mul__(self, other):
+    def __mul__(self, other : SeriesBase | sp.core.Expr) -> SeriesBase:
         if isinstance(other, Series):
             return other * self.a
+        elif isinstance(other, SeriesFactor):
+            return SeriesFactor(self.a * other.a)
         else:
             return SeriesFactor(self.a * other)
 
-    def __add__(self, other):
-        raise Exception("Cannot add to non-zero factor.")
+    def __add__(self, other : SeriesBase) -> SeriesBase:
+        # We could support this (see __getitem__) but it does not make sense.
+        raise Exception("Cannot add to SeriesFactor.")
 
-class SeriesEmpty:
+class SeriesEmpty(SeriesBase):
+    """ Empty series object with the only supported operation being addition.
+    """
+
     def __init__(self):
         pass
 
-    def __getitem__(self, n):
+    def __getitem__(self, n : int):
         raise Exception("No series item available for empty series.")
 
-    def __mul__(self, other):
+    def __mul__(self, other : SeriesBase | sp.core.Expr) -> SeriesBase:
         raise Exception("Multiplication not defined for empty series.")
 
-    def __add__(self, other):
+    def __add__(self, other : SeriesBase) -> SeriesBase:
         return other
 
 
